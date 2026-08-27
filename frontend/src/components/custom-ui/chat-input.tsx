73c4083f41b66c4a7a67de127649a67bbfc23700
Imran Suleiman <imrangreatest1@gmail.com>
2026-08-27 01:25:40 +0100

    Improve chat UI style

--- frontend/src/components/custom-ui/chat-input.tsx
+++ frontend/src/components/custom-ui/chat-input.tsx
@@ -1,24 +1,23 @@
-import { ArrowUpIcon } from "lucide-react";
+import { Plus, ArrowUpIcon } from "lucide-react";
 import { 
     InputGroup, 
     InputGroupAddon, 
     InputGroupInput,
     InputGroupButton
 } from "@/components/ui/input-group";
-import { Search } from "lucide-react";
 
 
-export function ChatInput({ onChange, onClick }: any) 
+export function ChatInput({ onChange, onClick, value }: any) 
 {
     return (
-        <InputGroup className="w-2xl">
-            <InputGroupInput onChange={onChange} placeholder="Search..." />
-            <InputGroupAddon>
-                <Search />
+        <InputGroup className="fixed bottom-0 left-1/2 -translate-x-1/2 bg-stone-900 w-3xl rounded-2xl p-2 border-stone-700 text-stone-400 hover:border-stone-600 has-[[data-slot=input-group-control]:focus]:border-stone-600 has-[[data-slot=input-group-control]:focus-visible]:ring-0">
+            <InputGroupInput onChange={onChange} value={value} placeholder="Search..."/>
+            <InputGroupAddon align="block-end">
+                <Plus className="text-stone-50" />
+                <InputGroupButton className="ml-auto rounded-xl px-2 py-4 bg-stone-600" onClick={onClick}>
+                    <ArrowUpIcon className="text-stone-50"/>
+                </InputGroupButton>
             </InputGroupAddon>
-            <InputGroupButton onClick={onClick}>
-                <ArrowUpIcon className="outline" />
-            </InputGroupButton>
         </InputGroup>
     )
 }
